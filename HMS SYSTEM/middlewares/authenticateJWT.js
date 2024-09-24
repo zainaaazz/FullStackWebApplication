9809b9213dfcf5786197accbf4bbc3fac7563e97
Thando1221 <142935890+Thando1221@users.noreply.github.com>
2024-09-24 15:54:42 +0200

    updated middleware

--- HMS SYSTEM/middlewares/authenticateJWT.js
+++ HMS SYSTEM/middlewares/authenticateJWT.js
@@ -1,27 +1,32 @@
 const jwt = require('jsonwebtoken');
 
-const authenticateJWT = (req, res, next) => {
-    // Extract the token from the Authorization header
-    const authHeader = req.headers['authorization'];
-    
-    if (authHeader) {
-        // The token format should be "Bearer <token>"
-        const token = authHeader.split(' ')[1];
+const authenticateJWT = (roles = []) => {
+    return (req, res, next) => {
+        const authHeader = req.headers['authorization'];
+        
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
 
-        if (token) {
-            jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-                if (err) {
-                    return res.status(403).json({ error: 'Forbidden' });
-                }
-                req.user = user;
-                next();
-            });
+            if (token) {
+                jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+                    if (err) {
+                        return res.status(403).json({ error: 'Forbidden' });
+                    }
+
+                    if (roles.length && !roles.includes(user.UserRole)) {
+                        return res.status(403).json({ error: 'Access denied' });
+                    }
+
+                    req.user = user;
+                    next();
+                });
+            } else {
+                return res.status(401).json({ error: 'Unauthorized: Bearer token missing' });
+            }
         } else {
-            return res.status(401).json({ error: 'Unauthorized: Bearer token missing' });
+            return res.status(401).json({ error: 'Unauthorized: Authorization header missing' });
         }
-    } else {
-        return res.status(401).json({ error: 'Unauthorized: Authorization header missing' });
-    }
+    };
 };
 
 module.exports = authenticateJWT;
