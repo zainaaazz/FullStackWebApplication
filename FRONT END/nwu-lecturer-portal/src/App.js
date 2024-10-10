aacef21091e74dd6126cec64be160ab11fddcd49
Thando1221 <142935890+Thando1221@users.noreply.github.com>
2024-10-10 15:44:06 +0200

    updated App js with create assignmnent link

--- FRONT END/nwu-lecturer-portal/src/App.js
+++ FRONT END/nwu-lecturer-portal/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Im
 import './App.css';
 import LoginForm from './components/LoginForm';
 import Assignments from './components/Assignments'; // Import the Assignments page
+import CreateAssignment from './components/CreateAssignment';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
        
         <Routes>
           <Route path="/" element={<LoginForm />} /> {/* Route for the login form */}
-          <Route path="/assignments" element={<Assignments />} /> {/* Route for the assignments page */}
+          <Route path="/assignments" element={<Assignments />} /> 
+          <Route path="/create-assignment" element={<CreateAssignment />} />
+          {/* Other routes can be added here */}
         </Routes>
       </div>
     </Router>
